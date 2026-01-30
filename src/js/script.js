f1de3366c29b9173dc4c182dad73cf44e88d0ef4
Syber-Tek <dellpc788@gmail.com>
2026-01-30 01:59:47 +0000

    update .gitignore and fix class names in index.html; enhance script.js with Swiper functionality and ScrollReveal effects; clean up style.css by removing unused classes

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-// alert(1);
 const navMobile = document.querySelector("#nav-mobile");
 const navTrigger = document.querySelector("#nav-trigger");
 const navClose = document.querySelector("#nav-close");
@@ -11,15 +10,64 @@ const toggleMenu = () => {
 navTrigger.addEventListener("click", toggleMenu);
 navClose.addEventListener("click", toggleMenu);
 
-//swiper
-const swiper = new Swiper(".mySwiper", {
+// Swiper for Projects
+const swiperProjects = new Swiper(".swiper-projects", {
+	loop: true,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
-	pagination: {
-		el: ".swiper-pagination",
-	},
+	pagination: false,
 });
 
-// console.log(navMobile)
+// Swiper for Testimonials
+const swiperTestimonials = new Swiper(".swiper-testimonials", {
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: false,
+});
+
+// Scroll Reveal
+const sr = ScrollReveal({
+	origin: 'bottom',
+	distance: '60px',
+	duration: 3000,
+	delay: 600,
+	// reset: true // Reset animation
+});
+
+// hero
+sr.reveal('.hero__title');
+sr.reveal('.hero__subtitle', { delay: 500 });
+sr.reveal('.hero__featuredImg', { delay: 700 });
+sr.reveal('.hero__mainImg', { delay: 700, origin: 'right' });
+sr.reveal('.hero__mainImg-border', { delay: 700, origin: 'right' });
+
+// about
+sr.reveal('.about__featuredImg', { delay: 300, origin: 'left' });
+sr.reveal('.about__stats', { delay: 400 });
+sr.reveal('.about__form', { delay: 500 });
+
+// services
+sr.reveal('.services__title');
+sr.reveal('.services__item', { interval: 100 });
+
+// services secondary
+sr.reveal('.services-secondary__title');
+sr.reveal('.services-secondary__img1', { origin: 'left' });
+
+// services tertiary
+sr.reveal('.service-tertiary__title');
+sr.reveal('.services-tertiary__imgs', { delay: 400 });
+sr.reveal('.services-tertiary__p', { delay: 400 });
+
+// projects
+sr.reveal('.projects h2');
+sr.reveal('.projects__slider');
+
+// testimonials
+sr.reveal('.testimonials__title');
+sr.reveal('.testimonials__slider');

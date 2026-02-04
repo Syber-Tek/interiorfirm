d539eb58feb70e59826acce5d47d462e4a23cf16
Syber-Tek <dellpc788@gmail.com>
2026-02-04 15:49:10 +0000

    refactor: update button class names, enhance ScrollReveal animations, and improve CSS utility classes

--- src/js/script.js
+++ src/js/script.js
@@ -32,42 +32,43 @@ const swiperTestimonials = new Swiper(".swiper-testimonials", {
 
 // Scroll Reveal
 const sr = ScrollReveal({
-	origin: 'bottom',
-	distance: '60px',
+	origin: "bottom",
+	distance: "60px",
 	duration: 3000,
 	delay: 600,
 	// reset: true // Reset animation
 });
 
 // hero
-sr.reveal('.hero__title');
-sr.reveal('.hero__subtitle', { delay: 500 });
-sr.reveal('.hero__featuredImg', { delay: 700 });
-sr.reveal('.hero__mainImg', { delay: 700, origin: 'right' });
-sr.reveal('.hero__mainImg-border', { delay: 700, origin: 'right' });
+sr.reveal(".hero__title");
+sr.reveal(".hero__subtitle", { delay: 500 });
+sr.reveal(".hero__featuredImg", { delay: 700 });
+sr.reveal(".hero__btn", { origin: "top", delay: 700 });
+sr.reveal(".hero__mainImg", { delay: 700, origin: "right" });
+sr.reveal(".hero__mainImg-border", { delay: 700, origin: "right" });
 
 // about
-sr.reveal('.about__featuredImg', { delay: 300, origin: 'left' });
-sr.reveal('.about__stats', { delay: 400 });
-sr.reveal('.about__form', { delay: 500 });
+sr.reveal(".about__featuredImg", { delay: 300, origin: "left" });
+sr.reveal(".about__stats", { delay: 400 });
+sr.reveal(".about__form", { delay: 500 });
 
 // services
-sr.reveal('.services__title');
-sr.reveal('.services__item', { interval: 100 });
+sr.reveal(".services__title");
+sr.reveal(".services__item", { interval: 100 });
 
 // services secondary
-sr.reveal('.services-secondary__title');
-sr.reveal('.services-secondary__img1', { origin: 'left' });
+sr.reveal(".services-secondary__title");
+sr.reveal(".services-secondary__img1", { origin: "left" });
 
 // services tertiary
-sr.reveal('.service-tertiary__title');
-sr.reveal('.services-tertiary__imgs', { delay: 400 });
-sr.reveal('.services-tertiary__p', { delay: 400 });
+sr.reveal(".service-tertiary__title");
+sr.reveal(".services-tertiary__imgs", { delay: 400 });
+sr.reveal(".services-tertiary__p", { delay: 400 });
 
 // projects
-sr.reveal('.projects h2');
-sr.reveal('.projects__slider');
+sr.reveal(".projects h2");
+sr.reveal(".projects__slider");
 
 // testimonials
-sr.reveal('.testimonials__title');
-sr.reveal('.testimonials__slider');
+sr.reveal(".testimonials__title");
+sr.reveal(".testimonials__slider");
